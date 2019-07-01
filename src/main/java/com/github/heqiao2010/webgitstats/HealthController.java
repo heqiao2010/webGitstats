19148d395b6dea54464f46ce1118f2ffb2da621b
qiao.he <qiao.he@qingteng.cn>
2019-07-01 14:54:23 +0800

    add semaphore example.

--- src/main/java/com/github/heqiao2010/webgitstats/HealthController.java
+++ src/main/java/com/github/heqiao2010/webgitstats/HealthController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class HealthController {
 
 	@RequestMapping(value = "/health", method = RequestMethod.GET)
-	public String health() {
+	public String health() throws InterruptedException{
+		TimeUnit.SECONDS.sleep(10);
 		return "hello world!";
 	}
 }
