30b0d6c003216b885b75ba7dd0df32a13af3ff92
Joel Herb <he_qiao_2010@yeah.net>
2020-03-30 19:09:32 +0800

    Update init_schema.sql

--- src/main/resources/db/init_schema.sql
+++ src/main/resources/db/init_schema.sql
@@ -6,10 +6,3 @@ addr char(256) NOT NULL COMMENT 'git address',
 status tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: init, 1: cloning, -1:error',
 create_time bigint(20) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS t_gitstats (
-id bigint(20) NOT NULL PRIMARY KEY,
-r_id bigint(20) NOT NULL COMMENT 'repository’s pk',
-dir_path char(128) NOT NULL COMMENT 'relative directory path',
-status tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: init, 1: generating statistic, -1:error'
-);
